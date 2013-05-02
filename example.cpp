4998218e9fa60fdd73c8723de47a23bdc6e2a9f2
Andrzej Borucki <borneq@example.com>
2013-05-02 17:05:31 +0200

    - play with setjmp/longjmp too.
    SVN Revision 7719
    Modified Tue Sep 5 15:03:30 2006 UTC by pluto

--- example.cpp
+++ example.cpp
@@ -1,21 +1,28 @@
 #include "backtracexx.hpp"
 
+#include <csetjmp>
 #include <csignal>
 #include <iostream>
 #include <iterator>
 #include <stdexcept>
 
+jmp_buf context;
+
 void signalHandler( int signalNumber )
 {
 	backtracexx::symbolic_backtrace_type s = backtracexx::symbols( backtracexx::scan() );
 	std::copy( s.begin(), s.end(), std::ostream_iterator< std::string >( std::cout, "\n" ) );
-	throw std::runtime_error( "fatality." );
+	longjmp( context, 1 );
 }
 
 void zoo()
 {
-	volatile int* p = 0;
-	*p = 0;
+	if ( setjmp( context ) == 0 )
+	{
+		volatile int* p = 0;
+		*p = 0;
+	}
+	throw std::runtime_error( "still alive?" );
 }
 
 void bar( void ( *f )() )
